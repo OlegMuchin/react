4446e59b666766ab240c11460a761a83e3b937f6
Oleg <122823069+OlegMuchin@users.noreply.github.com>
2023-11-10 19:29:04 +0300

    work

--- store/src/Components/Page/Stock/Product/Product.jsx
+++ store/src/Components/Page/Stock/Product/Product.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import Picture from '../../../UI/Picture/Picture';
 import MoreDetailsButton from '../../../UI/button/MoreDetailsButton/MoreDetailsButton';
 import Stars from '../../../UI/Stars/Stars';
+import "./Product.css"
 
 function Product({photo, priceNow, price, text, like, stars, ...props}) {
     return (
         <div className='Product'>
             <Picture src={photo}/>
-            <div>
-                <label>
-                    <span>{priceNow}</span>
-                    <span>{price}</span><br/>
-                    <span>{text}</span>
-                </label>
-                <Picture imgId="heart" src={like? "./img/icons/Heart_red.svg.png":"./img/icons/clarity_heart-line.svg"}/>
-                <Picture src="./img/icons/clarity_shopping-bag-line.svg"/>
+            <div className='Product_content'>
+                <div>
+                    <label>
+                        <span className='priceNow'>{priceNow}</span>
+                        <span className='price'>{price}</span><br/>
+                        {text}
+                    </label>
+                    <Picture imgId="heart" src={like? "./img/icons/Heart_red.svg.png":"./img/icons/clarity_heart-line.svg"}/>
+                    <Picture imgId="bag" src="./img/icons/clarity_shopping-bag-line.svg"/>
+                </div>
                 <MoreDetailsButton text="Подробнее"/>
                 <Stars amoutStars={stars}/>
             </div>
